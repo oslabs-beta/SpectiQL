0a8d2434dbe003c5003eee0d450142b651ff2b58
bigboydiamonds <57741810+bigboydiamonds@users.noreply.github.com>
2020-01-25 11:25:03 -0800

    first commit

---
+++ client/App.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { render } from 'react-dom'
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+    }
+        render() {
+            return (
+
+            )
+        }
+};
+
+export default App;
---
+++ index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './client/public/index.css'
+import App from './client/App.jsx'
+
+ReactDOM.render(<App />, document.getElementById('content'))
