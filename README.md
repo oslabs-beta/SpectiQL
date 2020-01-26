6005bf3cc2442c87760359e1377c1abb7d6d6ede
bigboydiamonds <57741810+bigboydiamonds@users.noreply.github.com>
2020-01-25 17:48:30 -0800

    webpack bundled, react injected into output.html

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # SpectiQL
+
+Current Working Scripts: 
+    'npm run build' will compile all React logic into output.html
+    'standard --run' will check for syntactical errors
--- client/App.jsx
+++ client/App.jsx
@@ -1,23 +1,22 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { render } from 'react-dom'
+import { render } from 'react-dom';
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            text: 'sodium bloat'
         };
     }
         render() {
             return (
-                <main>
-                    <Switch>
-                        
-                    </Switch>
-                </main>
+                <div>
+                    {this.state.text}
+                </div>
             )
         }
 };
 
 export default App;
+
