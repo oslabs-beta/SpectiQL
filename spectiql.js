0572a80a08289332d0c5ae95c8fb573923464b13
bigboydiamonds <57741810+bigboydiamonds@users.noreply.github.com>
2020-01-29 16:11:55 -0800

    changes to npm package 2.0

--- spectiql.js
+++ spectiql.js
@@ -5,30 +5,22 @@ const path = require('path');
 // function to render our frontend GUI depending on their endpoint
 const renderSpectiQL = (uri) => {
     return 
-`
- <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <link
-    rel="stylesheet"
-    href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-    crossorigin="anonymous"
-  />
+    
+    `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>SpectiQL</title>
-    <link rel="stylesheet" href="./styling/index.css">
-</head>
-<body>
-    <div></div>
-    <div id="root"></div>
-    <script type="text/javascript" src="../../build/bundle.js"></script>
-
-  <script type="text/javascript" src="bundle.js"></script></body>
-</html>
-`
+    <meta name="theme-color" content="#00c8c8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+    <div>hello!</div>
+    <script>const __gqlapi = ${JSON.stringify(uri)}</script>
+    // <script src="https://dxdw8zdwyvw2r.cloudfront.net/bundle4.js"></script>
+    </body>
+    </html>`
 
   }
   
