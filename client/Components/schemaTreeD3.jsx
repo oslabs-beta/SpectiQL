2cef9c18f9c6c0d74016679dbb806cd32a721c7a
bigboydiamonds <57741810+bigboydiamonds@users.noreply.github.com>
2020-02-10 17:44:35 -0800

    styling changes for better UI, with schematreed3 adjusted for schema, query, mutation roots

--- client/Components/schemaTreeD3.jsx
+++ client/Components/schemaTreeD3.jsx
@@ -1,14 +1,52 @@
 import React, { Component } from "react";
 import Tree from 'react-d3-tree';
 
-class schemaTreeD3 extends Component {
+class SchemaTreeD3 extends Component {
     constructor(props) {
         super(props);
     };
 
     render() {
+
+        function convertSchema(object) {
+            for (let key in object) {
+              if (typeof(object[key]) == "object") {
+                let obj = convertJSON(object[key]);
+                obj.key = obj.name ? obj.name : "";
+                if (obj.key === "") {
+                  obj.name = key;
+                } else {
+                  obj.name = obj.key;
+                }
+                if (!object["children"])
+                  object["children"] = [];
+                object["children"].push(obj);
+                delete object[key];
+              }
+            }
+            return object;
+          }
         //below will convert all of the schema json data
-        function convertJSON(object) {
+        function convertQuerySchema(object) {
+            for (let key in object) {
+              if (typeof(object[key]) == "object" && object[key].type !== "InputObjectTypeDefinition") {
+                let obj = convertJSON(object[key]);
+                obj.key = obj.name ? obj.name : "";
+                if (obj.key === "") {
+                  obj.name = key;
+                } else {
+                  obj.name = obj.key;
+                }
+                if (!object["children"])
+                  object["children"] = [];
+                object["children"].push(obj);
+                delete object[key];
+              }
+            }
+            return object;
+          }
+
+          function ConvertMutationSchema(object) {
             for (let key in object) {
               if (typeof(object[key]) == "object" && object[key].type !== "InputObjectTypeDefinition") {
                 let obj = convertJSON(object[key]);
@@ -27,7 +65,8 @@ class schemaTreeD3 extends Component {
             return object;
           }
           
-          let queryTreeData = convertJSON(this.props.schema);
+          let schemaTreeData = convertSchema(this.props.schema);
+          let queryTreeData = convertQuerySchema(this.props.schema);
           console.log(queryTreeData);
 
         return (
@@ -38,4 +77,4 @@ class schemaTreeD3 extends Component {
     }
 }
 
-export default schemaTreeD3;
+export default SchemaTreeD3;
