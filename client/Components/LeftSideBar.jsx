9078788a3dd92c9f9349a7cd12332b6eda1a60dd
bigboydiamonds <57741810+bigboydiamonds@users.noreply.github.com>
2020-02-11 22:13:50 -0800

    styling changes to be consistent

--- client/Components/LeftSideBar.jsx
+++ client/Components/LeftSideBar.jsx
@@ -10,33 +10,39 @@ class SideBar extends Component {
   render () {
     return (
       <div className='mainNavBar'>
+
         <ButtonToolbar className='buttonContainer'>
-          <Link to="/queries">
+
             <Button
               onClick={this.props.queryPage}
               className="queries"
               variant="outline-dark"
               size="lg"
             >
+              <Link to="/queries">
               Queries
+              </Link>
             </Button>
-          </Link>
-          <Link to="/mutations">
+
             <Button
               onClick={this.props.mutationPage}
               className="mutations"
               variant="outline-dark"
               size="lg"
             >
+              <Link to="/mutations">
               Mutations
+              </Link>
             </Button>
-          </Link>
+
           <Button className='subscriptions' variant='outline-dark' size='lg'>
             Subscriptions
           </Button>
+
           <Button className='export' onClick={this.props.handleExportClick} className="export" variant="outline-dark" size="lg">
             Export
           </Button> 
+          
         </ButtonToolbar>
       </div>
     )
